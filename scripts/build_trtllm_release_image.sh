91dc3849b2bc47d2b446dccc7cae9c29d90abad0
Bryce Dubayah <bdubayah@gmail.com>
2026-07-08 13:39:43 -0700

    [None][ci] fix Depot release build timeout (#176)
    Signed-off-by: Bryce Dubayah <bdubayah@gmail.com>

--- scripts/build_trtllm_release_image.sh
+++ scripts/build_trtllm_release_image.sh
@@ -6,9 +6,12 @@ export BUILDX_NO_DEFAULT_ATTESTATIONS=1
 
 commit_sha="$(git rev-parse HEAD)"
 image_tag="baseten/tensorrt_llm-release:${commit_sha:0:10}"
+
 CUDA_ARCHS_AMD64="${CUDA_ARCHS_AMD64:-80-real;86-real;90-real;100-real;103-real}"
 CUDA_ARCHS_ARM64="${CUDA_ARCHS_ARM64:-100-real;103-real}"
-build_wheel_opts="-D CMAKE_CXX_COMPILER_LAUNCHER=sccache -D CMAKE_CUDA_COMPILER_LAUNCHER=sccache"
+BUILD_WHEEL_JOBS="${BUILD_WHEEL_JOBS:-16}"
+
+build_wheel_opts="-j ${BUILD_WHEEL_JOBS} -D CMAKE_CXX_COMPILER_LAUNCHER=sccache -D CMAKE_CUDA_COMPILER_LAUNCHER=sccache"
 docker_build_opts="--pull --push --platform linux/amd64,linux/arm64"
 docker_build_args="--secret id=SCCACHE_WEBDAV_TOKEN,env=SCCACHE_WEBDAV_TOKEN"
 docker_build_args="${docker_build_args} --build-arg CUDA_ARCHS_AMD64=\"${CUDA_ARCHS_AMD64}\""
